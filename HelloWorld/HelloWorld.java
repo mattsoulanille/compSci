ea7717d9bb1b679e2a3edb42a90c4dddd7166cbc
Matthew Soulanille <matthew@Rime.local>
2014-09-05 11:52:41 -0700

    AP CS repo started.

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+HelloWorld.java
+@author Matthew Soulanille
+@version 2014-09-02
+*/
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+	System.out.println("Hello, World!");
+    }
+}
---
+++ HelloWorld/HelloWorld2.java
@@ -0,0 +1,16 @@
+/**
+ *HelloWorld2.java
+ *@author Matthew Soulanille
+ *@version 2014-09-04
+ */
+
+class HelloWorld2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+        System.out.println("Goodbye Matthew Soulanille");
+
+    }
+}
